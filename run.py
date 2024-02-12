989ba559080c6399331eb6010800c1bf9e7e6070
Maksim Emarov <xnneso@gmail.com>
2024-02-12 10:40:43 +0500

    fix

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 def main(input: str):
     arr = input.split(' ')
+
     if len(arr) != 3:
         return Exception('Неверный формат данных')
+    
     try:
         a = int(arr[0])
         b = int(arr[2])
     except:
         return Exception('Данные должны быть числами')
+    
+    if arr[1] not in ['+', '-', '*', '/']:
+        return Exception('Неверный оператор')
+    
     if 0 < a < 11 and 0 < b < 11:
         operation = {
             '+': a+b,
